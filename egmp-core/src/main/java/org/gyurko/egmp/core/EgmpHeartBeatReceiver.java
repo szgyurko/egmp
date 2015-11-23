47bb8018260bfd26f5e4b1c3dfdf4f9ac4426a53
Szabolcs Gyurko <szabolcs.gyurko@rexel.co.uk>
2015-11-23 14:35:37 +0000

    Added license

--- egmp-core/src/main/java/org/gyurko/egmp/core/EgmpHeartBeatReceiver.java
+++ egmp-core/src/main/java/org/gyurko/egmp/core/EgmpHeartBeatReceiver.java
@@ -4,8 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
- * @author Szabolcs Gyurko <szabolcs@gyurko.org>
+ EGMP - Extensible Group Management Protocol
+
+ Copyright (C) 2013-2015  Szabolcs Gyurko <szabolcs@gyurko.org>
+
+ This file is part of EGMP project.
+
+ EGMP is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, version 2 of the License, but not
+ any later version.
+
+ EGMP is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with EGMP.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class EgmpHeartBeatReceiver implements Runnable {
     /** Class level logging */
--- egmp-elevation-strategies/src/main/java/org/gyurko/egmp/elevation/IpV6AddressElevationStrategy.java
+++ egmp-elevation-strategies/src/main/java/org/gyurko/egmp/elevation/IpV6AddressElevationStrategy.java
@@ -7,13 +7,29 @@ import java.net.*;
 import java.util.Enumeration;
 
 /**
- *
- * @author Szabolcs Gyurko <szabolcs@gyurko.org>
+ EGMP - Extensible Group Management Protocol
+
+ Copyright (C) 2013-2015  Szabolcs Gyurko <szabolcs@gyurko.org>
+
+ This file is part of EGMP project.
+
+ EGMP is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, version 2 of the License, but not
+ any later version.
+
+ EGMP is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with EGMP.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class IpV6AddressElevationStrategy extends IpAddressElevationStrategy {
     /** Class level logging */
     private static final Logger LOGGER = LoggerFactory.getLogger(IpV6AddressElevationStrategy.class);
-    /** bytes in java are signed, therefore if a negative number comes out, we have to do binary complement operation */
+    /** bytes in java are signed, therefore if a negative number comes out, we have to compensate */
     private static final long BYTE_OVERFLOW_VALUE = 127;
 
     @Override
